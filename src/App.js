0d7bbcf84e5325f7f8dfe6404ae5193dd272d528
Xujuan.Chen <xujuan.chen@lc.cuny.edu>
2020-09-26 16:05:58 -0400

    final commit, I will become a React pro!

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/xujuanchen2020"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 
+          Check Me Out!
         </a>
       </header>
     </div>
