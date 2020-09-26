da13adacae1cd752cc0e7ea0a015aba9cac83b4c
Xujuan.Chen <xujuan.chen@lc.cuny.edu>
2020-09-26 15:45:58 -0400

    modified commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My name is Xujuan Chen and I'm going to master React!
+          {/* Edit <code>src/App.js</code> and save to reload. */}
         </p>
         <a
           className="App-link"
